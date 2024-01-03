ee4b84c8a20dbc002c7ebe0b1397e0e125e1477f
Franco Maidana <108634791+franco-maidana@users.noreply.github.com>
2024-01-02 23:30:53 -0300

    Endpoint server.js

--- data/server.js
+++ data/server.js
@@ -5,4 +5,10 @@ const server = express();
 const PORT = 8080;
 const ready = () => console.log("server ready on port" + PORT);
 
+// Endpoint
+const ruta = "/";
+const funcionQueVaALeer = (req, res) => {
+  return res.status(200).send("My first express Server");
+};
+
 server.listen(PORT, ready);
