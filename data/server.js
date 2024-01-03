6c68698654cb46868bffd4453d4aa09f8fbd5da4
franco-maidana <tomedelrio@gmail.com>
2024-01-02 23:21:19 -0300

    se agrego los Endpoint

--- data/server.js
+++ data/server.js
@@ -6,3 +6,11 @@ const PORT = 8080;
 const ready = () => console.log("server ready on port" + PORT);
 
 server.listen(PORT, ready);
+
+// Endpoiont
+const ruta = "/";
+const funcionQueVaALeer = (req, res) => {
+  return res.status(200).send("My first express Server");
+};
+
+server.get(ruta, funcionQueVaALeer);
