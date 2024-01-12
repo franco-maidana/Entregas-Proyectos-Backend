b99bca0c5685475198d40c155ba0bbdd198d6f86
franco-maidana <tomedelrio@gmail.com>
2024-01-12 00:25:45 -0300

    termiando el proyecto

--- Primera-Entrega/server/src/router/index.router.js
+++
@@ -1,9 +0,0 @@
-// sitaccis de todo enrutador principal
-import { Router } from "express";
-import apiRouter from "./api/index.router.js"; // enrutador de la api
-
-const router = Router();
-
-router.use("/api", apiRouter);
-
-export default router;
